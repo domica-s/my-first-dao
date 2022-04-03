c05a4a919e1f1c79644c544ac7f33dd791c2915d
Domica <1155130940@link.cuhk.edu.hk>
2022-04-03 17:04:06 +0800

    third commit

---
+++ hardhat-tutorial/constants/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x543feff81247f1cf1836c1fb6585ac2cee33873c";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
